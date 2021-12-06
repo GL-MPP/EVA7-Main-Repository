A new custom Resnet model is created. Main.py and Utils.py are updated to cater to fix previous errors and include calculation and rules to implement One cycle policy LR scheduler and train custom resnet model.

main.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 
@@ -12,113 +13,126 @@ from torchsummary import summary
 import os
 import argparse
 from tqdm import tqdm
-import utils
-from utils import *
 import resnet
 from resnet import ResNet18
-
+import custom_resnet
+from custom_resnet import Create_Model
+import utils
+from utils import *
 
 # Data
-def prep_data(p_batchsize = 64, p_download_required = 'Y'
-                           ,p_orig_train_dataset = None
-                           ,p_orig_test_dataset = None):
+def prep_data(p_batchsize = 64):
   cuda = torch.cuda.is_available()
-  if (p_download_required == 'Y'):
-    print('==> Downloading data..')
-    train_transform = transforms.Compose([transforms.ToTensor()])
+  from albumentations import (pytorch,Normalize,Cutout,Crop)
+  
+  print('==> Downloading data..')
+  trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=None)
 
-    test_transform = transforms.Compose(
-        [transforms.ToTensor()])
+  testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=None)
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=train_transform)
+  print('==> Preparing Transforms and Augmentation rules')
+  train_transform = transforms.Compose([transforms.ToTensor()
+    ,transforms.RandomCrop(size = (32,32),padding = 4,padding_mode='edge')
+    ,transforms.RandomHorizontalFlip(p=0.5)
+    ])
+  
+  test_transform = transforms.Compose([transforms.ToTensor()])
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=test_transform)
+  trainset.transform = train_transform  
+  testset.transform = test_transform
+  
+  # The torch vision transforms is followed by albumentation transforms
+  augmented_trainset = Augmentation_TrainDataset(trainset)
+  augmented_testset = Augmentation_TestDataset(testset)
+  #augmented_trainset = trainset
+  #augmented_testset = testset
 
-    
-    train_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((trainset.data.mean(),),(trainset.data.std(),))])
-
-    test_transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((testset.data.mean(),),(testset.data.std(),))])
-    
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=train_transform)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=test_transform)
-    classes = ('plane', 'car', 'bird', 'cat', 'deer',
-             'dog', 'frog', 'horse', 'ship', 'truck')
-    augmented_trainset = trainset
-    augmented_testset = testset
-    dataloader_args = dict(shuffle=True, batch_size=p_batchsize)
-    train_loader = torch.utils.data.DataLoader(trainset, **dataloader_args)
-    dataloader_args = dict(shuffle=False, batch_size=p_batchsize)
-    test_loader = torch.utils.data.DataLoader(testset, **dataloader_args)
-  else:
-    print('==> Preparing augmented Dataset...')
-    augmented_trainset = Augmentation_TrainDataset(p_orig_train_dataset)
-    augmented_testset = Augmentation_TestDataset(p_orig_test_dataset)
-
-    print('==> Preparing Train/Test loaders')
-    dataloader_args = dict(shuffle=True, batch_size=p_batchsize,num_workers=4, pin_memory=True) if cuda else dict (shuffle=True, batch_size=p_batchsize)
-    train_loader = torch.utils.data.DataLoader(augmented_trainset, **dataloader_args)
-    dataloader_args = dict(shuffle=False, batch_size=p_batchsize,num_workers=4, pin_memory=True) if cuda else dict (shuffle=False, batch_size=p_batchsize)
-    test_loader = torch.utils.data.DataLoader(augmented_testset, **dataloader_args)
-  return augmented_trainset, augmented_testset,train_loader, test_loader
+  print('==> Preparing Train/Test loaders')
+  dataloader_args = dict(shuffle=True, batch_size=p_batchsize,num_workers=2, pin_memory=True) if cuda else dict (shuffle=True, batch_size=p_batchsize)
+  train_loader = torch.utils.data.DataLoader(augmented_trainset, **dataloader_args)
+  
+  dataloader_args = dict(shuffle=False, batch_size=p_batchsize,num_workers=2, pin_memory=True) if cuda else dict (shuffle=False, batch_size=p_batchsize)
+  test_loader = torch.utils.data.DataLoader(augmented_testset, **dataloader_args)
+  return train_loader, test_loader,testset
 
 
 # Initialize model
-def initialize_model(p_lr = 0.01,p_momentum = 0.9):
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
-  print('==> Initializing Model...')
-  model = ResNet18().to(device)
-  print('==> Model initilaized on ',device)
-  print('==> Initializing Optimizer...')
-  optimizer = optim.SGD(model.parameters(), lr=p_lr, momentum=p_momentum)
-  print('==> SGD optimizer initialized')
-  summary(model, input_size=(3, 32, 32))
-  return model, optimizer, device
+def initialize_model(p_model = None, p_lr = 0.01,p_momentum = 0.9, p_train_mode = 'N', p_train_loss_lst = [], p_lr_lst = []):
+  train_loss=[]
+  if (p_train_mode!='N' and (len(p_train_loss_lst)==0 or len(p_lr_lst)==0)):
+    print('Error in parameters...Cannot set LR scheduler without MAX LR.\nModel not initialized..')
+  else:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('==> Initializing Model...')
+    model = p_model.to(device)
+    print('==> Model initilaized on ',device)
+    print('==> Initializing Optimizer...')
+    optimizer = optim.Adam(model.parameters(), lr=p_lr)#,momentum = .9)
+    print('==> Adam optimizer initialized')
+    if(p_train_mode=='N'):
+      lr_sch = lr_scheduler.StepLR(optimizer=optimizer, step_size=1, gamma=1.069)
+      print('==> LR Scheduler initialized to StepLR to find the max LR with min Loss')
+    else:
+      train_loss = [x for x in p_train_loss_lst if np.isnan(x) == False]
+      train_loss.reverse()
+      l_max_lr = p_lr_lst[len(train_loss) - train_loss.index(min(train_loss)) -1]
+      print('==> Max LR dervied for Min Loss = ', .09)
+      lr_sch = lr_scheduler.OneCycleLR(optimizer,max_lr=.09,pct_start=6/24,total_steps=24,three_phase=False)
+      print('==> OCP Scheduler initialized with Max LR -',.09)
+    summary(model, input_size=(3, 32, 32))
+    return model,optimizer, device, lr_sch
 
 
 # Train Test Model
-def train_model(model, device, train_loader, optimizer, epoch):
+def train_model(model, device, train_loader, optimizer, epoch,
+                p_train_mode ='N',p_lr_scheduler= None):
   from tqdm import tqdm
+  
   criterion = nn.CrossEntropyLoss()
   train_losses = []
   train_acc = []
   lst = []
-  lambda_l1 = 0.000001
+  lr_lst = []
   pbar = tqdm(train_loader)
   correct = 0
   processed = 0
-  lst = []
-  torch.set_grad_enabled(True)
+  #torch.set_grad_enabled(True)
   model.train()
+  #lr = p_starting_lr
   for batch_idx, (data, target) in enumerate(pbar):
-    # get samples
+    ################################
+    ##Find the MAX lr
+    if (p_train_mode == 'N'):
+      p_lr_scheduler.step()
+      lr_lst.append(p_lr_scheduler.get_last_lr())
+    #################################
     
+        
     data, target = data.to(device), target.to(device)
-    
     # Init
     optimizer.zero_grad()
     y_pred = model(data)
     loss = criterion(y_pred, target)
     train_losses.append(loss.item())
-    # Backpropagation
+    # Backward
     loss.backward()
-    
     optimizer.step()
-    
+    if (loss > 5000):
+      exit
     # Update pbar-tqdm
     pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
     correct += pred.eq(target.view_as(pred)).sum().item()
     processed += len(data)
     
     pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-    train_acc.append(100*correct/processed)
+    
     #if (batch_idx >5):
       #break
-  lst = [train_losses,train_acc]
+  train_acc.append(100*correct/processed)
+  
+  lst = [train_losses,train_acc,lr_lst]
   return(lst)
 
 def test_model(model, device, test_loader):
@@ -126,7 +140,7 @@ def test_model(model, device, test_loader):
   test_losses = []
   test_acc = []
   lst = []
-  lambda_l1 = 0.000001
+  
   model.eval()
   test_loss = 0
   correct = 0
@@ -138,41 +152,71 @@ def test_model(model, device, test_loader):
       data, target = data.to(device), target.to(device)
       
       output = model(data)
-      #F.nll_loss
       test_loss += criterion(output, target)#, reduction='sum').item()  # sum up batch loss
       pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+      
       correct += pred.eq(target.view_as(pred)).sum().item()
+      
       count += 1
       #if (count > 5):
         #break
       test_loss /= len(test_loader.dataset)
-      test_losses.append(test_loss)
-      test_acc.append(100. * correct / len(test_loader.dataset))
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    test_losses.append(test_loss)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    
-    lst = [test_losses,test_acc]
+  
+  lst = [test_losses,test_acc]
   return(lst)
 
-
+def prepare_data(p_batch_size = 64):
+  print('==> Preparing Dataset..')
+  train_loader,test_loader,test_set = prep_data(p_batchsize=p_batch_size) 
+  return train_loader,test_loader,test_set
 
 # Run Train/Test Epoch Loops
-def run_train_test_epochs(p_epochs = 1, p_batch_size =1, p_model = None, p_optimizer = None, p_device = 'cpu'):
+def run_train_test_epochs(p_epochs = 1, p_batch_size =1, p_model = None
+                        ,p_optimizer = None, p_device = 'cpu'
+                        ,p_train_mode = 'N',p_lr_scheduler = None
+                        ,p_train_loader = None, p_test_loader = None):
   train_epoch_lst = []
   test_epoch_lst = []
-  #model = p_model.to(p_device)
-  for epoch in range(p_epochs):
-    if (epoch == 0):
-      orig_train, orig_test, train_loader , test_loader = prep_data(p_batchsize=p_batch_size, p_download_required='Y')
-    else:
-      print('\n==> Refreshing Dataset for next epoch')
-      temp_a,temp_b, train_loader , test_loader = prep_data(p_batchsize=p_batch_size, p_download_required='N',p_orig_train_dataset = orig_train,p_orig_test_dataset = orig_test)
-      print("==> Train loop for Epoch #:", epoch)
-      train_epoch_lst = train_model(p_model, p_device, train_loader, p_optimizer, p_epochs)
-      print("==> Test loop for Epoch #:", epoch)
-      test_epoch_lst = test_model(p_model, p_device, test_loader)
-    
-  return train_epoch_lst, test_epoch_lst
+  lr_lst = []
+  train_loss = []
+  train_acc = []
+  test_loss = []
+  test_acc = []
+  lr_ocp = []
+  l_max_lr = 0
+  l_min_lr = 0
+  l_epoch = 0
+  
+  if (p_train_mode == 'N'):
+    l_epoch = 1 # Run only for 1 epoch to calculate MAX LR
+  else:
+    l_epoch = p_epochs
+  
+  
+  for epoch in range(l_epoch):
+      ## If train mode is N it is being used to find max LR
+      if (p_train_mode != 'N'):
+        p_lr_scheduler.step()
+        print('\n==> Setting One cycle policy LR rate for optimizer. LR set to ',p_lr_scheduler.get_last_lr())
+
+      print("\n==> Train loop for Epoch #:", epoch + 1)
+      train_epoch_lst = train_model(p_model, p_device, p_train_loader, p_optimizer, p_epochs
+                                  ,p_train_mode=p_train_mode, p_lr_scheduler = p_lr_scheduler)
+      ## If train mode is N no need for test validations
+      if (p_train_mode != 'N'):
+        print("==> Test loop for Epoch #:", epoch)
+        test_epoch_lst = test_model(p_model, p_device, p_test_loader)
+      train_loss += train_epoch_lst[0]
+      train_acc += train_epoch_lst[1]
+      lr_lst += train_epoch_lst[2]
+      if (p_train_mode != 'N'):
+        test_loss += test_epoch_lst[0]
+        test_acc += test_epoch_lst[1]
+  return train_loss, train_acc,test_loss,test_acc,lr_lst
 
 

utils.py
@@ -9,6 +9,7 @@
 
 import os
 import sys
+import subprocess
 import time
 import math
 import torch
@@ -19,8 +20,16 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset
 import albumentations as A
-from albumentations import (pytorch,Normalize,Cutout,Crop)
+from albumentations import (pytorch,Normalize,Cutout,Crop,CoarseDropout)
 import matplotlib.pyplot as plt
+
+
+def install_pkg():
+  subprocess.call([sys.executable, "-m", "pip", "install", "multipledispatch"])
+  subprocess.call([sys.executable, "-m", "pip", "install", "grad-cam"])
+  subprocess.call([sys.executable, "-m", "pip", "install", "cam"])
+  subprocess.call([sys.executable, "-m", "pip", "install", "albumentations==0.4.6"])
+
 
 class Augmentation_TrainDataset(Dataset):
     def __init__(self, original_dataset):
@@ -28,12 +37,10 @@ class Augmentation_TrainDataset(Dataset):
         self.mean = self.original_dataset.data.mean()
         self.std = self.original_dataset.data.std()
         self.max = self.original_dataset.data.max()
+        self.fill = -1.866605 # Checking for test run
         self.aug = A.Compose({
-        #A.VerticalFlip(p=1),
-        #A.PadIfNeeded(min_height=40, min_width=40, always_apply = True),
-        A.RandomCrop(height = 32, width = 32, always_apply=False, p=1.0),
-        A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, always_apply=False, p=1,fill_value = (.5-self.mean)/self.std)
-        #A.Normalize((self.mean,), (self.std,),self.max),
+        A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.fill, always_apply=False, p=1),
+        A.Normalize(mean = (0.5,), std = (0.5,))
         })
          
     def __len__(self):
@@ -42,9 +49,19 @@ class Augmentation_TrainDataset(Dataset):
     def __getitem__(self, i):
         data_item = self.original_dataset[i]
         img, lbl = data_item
+        self.fill =  img.mean() 
+        self.mean = np.array(img.mean()) 
+        self.std = np.array(img.std())
+        self.max = np.array(img.max()) 
+        #self.aug = A.Compose({
+        #A.CoarseDropout(max_holes=1, max_height=8, max_width=8, always_apply=False, p=.5,fill_value = self.fill)})
+        self.aug = A.Compose({
+        A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=self.fill, always_apply=False, p=0.5),
+        A.Normalize(mean = (self.mean,), std = (self.std,), max_pixel_value = self.max)
+        })
         aug_img = self.aug(image=np.transpose(np.array(img), (1, 2, 0)))['image']
-        #aug_img = self.aug(image = img)['image']
         aug_img = np.transpose(np.array(aug_img), (2,0,1))
+        aug_img = torch.tensor(aug_img)
         data_item = (aug_img,lbl)    
         return data_item
 
@@ -55,7 +72,7 @@ class Augmentation_TestDataset(Dataset):
         self.std = self.original_dataset.data.std()
         self.max = self.original_dataset.data.max()
         self.aug = A.Compose({
-        #A.Normalize((self.mean,), (self.std,),self.max),
+        A.Normalize((self.mean,), (self.std,),self.max)
         })
          
     def __len__(self):
@@ -64,25 +81,69 @@ class Augmentation_TestDataset(Dataset):
     def __getitem__(self, i):
         data_item = self.original_dataset[i]
         img, lbl = data_item
+        self.mean = np.array(img.mean()) 
+        self.std = np.array(img.std())
+        self.max = np.array(img.max())
+        self.aug = A.Compose({
+        A.Normalize(mean = (self.mean,), std = (self.std,), max_pixel_value = self.max)
+        })
         aug_img = self.aug(image=np.transpose(np.array(img), (1, 2, 0)))['image']
         aug_img = np.transpose(np.array(aug_img), (2,0,1))
+        aug_img = torch.tensor(aug_img)
         data_item = (aug_img,lbl)    
         return data_item
 
-def display_graphs(train_lst = [], test_lst = []):
-  print('Loss and Accuracy graphs')
-  fig, axs = plt.subplots(2,2,figsize=(15,10))
-  axs[0, 0].plot(train_lst[0])
-  axs[0, 0].set_title("Training Loss")
-  axs[1, 0].plot(train_lst[1])
-  axs[1, 0].set_title("Training Accuracy")
-  axs[0, 1].plot(test_lst[0])
-  axs[0, 1].set_title("Test Loss")
-  axs[1, 1].plot(test_lst[1])
-  axs[1, 1].set_title("Test Accuracy")
+def display_graphs(train_loss,train_acc,test_loss,test_acc,lr_range):
+    
+  if (len(lr_range)>0):
+    plt.plot(np.log(lr_range),train_loss)
+    plt.xlabel('LR')
+    plt.ylabel('Training Loss')
+    plt.title('LR vs Training Loss')
+    plt.show()
+  else:
+    print('Loss and Accuracy graphs')
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_loss)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_loss)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+    plt.show()
+    print('LR Batch Test')
+    #fig, axs = plt.subplots(figsize=(15,10))
+
+def gen_heatmap (p_img_list = None, p_model = None, p_target_layers = None,p_category = 1):
+  import cam
+  from pytorch_grad_cam.base_cam import BaseCAM
+  import pytorch_grad_cam
+  from pytorch_grad_cam import GradCAM
+  hm_img_lst = []
+  #cpu_model = p_model.to('cpu')
+  cam = GradCAM(model=p_model, target_layers=p_target_layers)
+  for i in range(len(p_img_list)):
+    input_img = torch.tensor(p_img_list[i])
+    input_img = input_img.reshape(1,3,32,32)
+    hm_img = cam(input_tensor= input_img,target_category= p_category)
+    hm_img = torch.tensor(hm_img)
+    hm_img_lst.append(hm_img.reshape(32,32))
+    #gen_heatmap(img_lst,model,model.part_layer1)
+  if len(hm_img_lst) > 0:
+    print('\n Gradcam images for above images as below \n')
+    for i in range(len(hm_img_lst)):
+      plt.subplot(2, 5, i+1)
+      plt.axis('off')
+      plt.imshow(hm_img_lst[i])
+  else:
+    print('Gradcam not generated...')
 
 def gen_gradcam(p_img_list = None, p_model = None):
   img_lst = []
+  
+  # Custom code
   gradcam_img = torch.tensor([])
   cpu_model = p_model.to('cpu')
   for i in range(len(p_img_list)):
@@ -124,40 +185,37 @@ def gen_gradcam(p_img_list = None, p_model = None):
     #plt.show()
 
 def find_misclassified_img(p_images = 10, p_model = None, 
-                           p_device = 'cpu', p_test_loader = None,
-                           p_max = 0, p_mean = 0, p_std = 0):
+                           p_device = 'cpu', p_test_loader = None,p_test_set = None):
   print('Misclassified images for Batch Normalization')
   cpu_model = p_model.to(p_device)
   img_counter = 0;
   i = 0;
-  img_lst = []
+  img_lst=[]
+  ret_img_lst=[]
   for test_batch in p_test_loader:
-    i += 1
-    print(f'Checking batch # {i}')
+    print(f'Checking batch # {i+1}')
     #test_batch = next(iter(test_loader)) # Get the first batch from the train loader
     batch_img, batch_lbl = test_batch
     for j in range(len(batch_img)): # processing sets of images in each batch
       img, lbl = batch_img.data[j].to(p_device), batch_lbl.data[j].to(p_device)
-      #img, lbl = batch_img.data[j], batch_lbl.data[j]
-      #img, lbl = img.to(device), lbl.to(device)
-      img1 = img*p_std+p_mean
+      img1 = p_test_set.data[i+j] 
       test_pred = cpu_model(img.reshape(1,3,32,32)) # passing each test image to the model
       predicted_label = test_pred.reshape(-1,10).argmax(dim=1).item() # finding the max tensor position to determine the predicted label 
       if (predicted_label != lbl): # Check to see if the model prediction was correct and print 10 misclassified images
         img_counter += 1
-        plt.imshow(img1.permute(1,2,0))
+        plt.figure(figsize = (2,2))
+        plt.imshow(img1)
         plt.show()
-        print(f'The model predicted the above image as - {predicted_label}')
-        print(f'Actual label of the above image is - {lbl}')
+        print(f'The model predicted the above image as - {p_test_set.classes[predicted_label]}')
+        print(f'Actual label of the above image is - {p_test_set.classes[lbl]}')
 
-        temp_img = img1.permute(1,2,0)
-        img_lst.append(temp_img)
-        #plt.subplot(5, 2, 10)
-        #plt.axis('off')
-        #plt.imshow(img1.numpy().squeeze(), cmap='gray_r')
+        temp_img = img1
+        img_lst.append(torch.tensor(temp_img))
+        ret_img_lst.append(img)
 
         if (img_counter > p_images-1):
           break
+    i+=1
     if (img_counter > p_images-1):
       print(f'10 misclassified images found.. Processing stopped')
       break
@@ -167,5 +225,6 @@ def find_misclassified_img(p_images = 10, p_model = None,
     plt.axis('off')
     plt.imshow(img_lst[i].squeeze())
   plt.show()
-  return img_lst
+  return ret_img_lst
+
 